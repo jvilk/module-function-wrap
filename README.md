48ca008f8242dc35d3a9e746306aea0751c0fa47
John Vilk <jvilk@cs.umass.edu>
2015-10-15 15:43:18 -0400

    First untested version.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # object-wrapper
 > Wraps all of an object's functions in a wrapper of your choosing.
+
+### Examples
+
+Wrap the [`assert`](https://github.com/defunctzombie/commonjs-assert) library to appropriately throw exceptions through the [Mocha](https://mochajs.org/) test runner:
+
+```{js}
+var wrapper = require('object-wrapper');
+var mocha = require('mocha');
+var assert = wrapper(require('assert'), function (fcnInfo, args) {
+    try {
+        fcnInfo.originalFcn.apply(fcnInfo.namespace, args);
+    } catch (e) {
+        mocha.throwError(e);
+    }
+}, 'assert' /* Name to use for root object in fcnInfo objects */, true /* Attach wrappers to original object */);
+```
+
+### Limitations
+
+Currently limited to wrapping objects without prototypes, and functions that are not constructors.
+These limitations are not unsurmountable, and may be addressed in a future update (and I welcome PRs!).
--- index.ts
+++ index.ts
@@ -12,6 +12,22 @@ interface IFunctionInfo {
     originalFcnName: string;
 }
 
-export = function wrapModule(wrapper: (thisArg: any, originalFcn: any, )) {
-
+export = function wrapObject(obj: any, wrapper: (functionInfo: IFunctionInfo, args: IArguments) => void, objName: string = 'root', modifyObj: boolean = false): any {
+    if (typeof(obj) !== 'object') {
+        throw new Error(`Erroneous argument of type ${typeof(obj)}. object-wrapper only wraps objects.`);
+    }
+    var wrappedObj: any = modifyObj ? obj : {};
+    // Avoid items on the prototype.
+    Object.keys(obj).forEach(function(name) {
+        var funcInfo: IFunctionInfo = {
+            namespace: obj,
+            namespaceName: objName,
+            originalFcn: obj[name],
+            originalFcnName: name
+        };
+        wrappedObj[name] = function() {
+            wrapper(funcInfo, arguments);
+        };
+    });
+    return wrappedObj;
 };
